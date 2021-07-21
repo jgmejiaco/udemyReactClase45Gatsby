ed69c3500a7b96c6a4ba4e82fcd5fc151bf9bce8
Juan Guillermo <jgmejiaco@gmail.com>
2021-07-21 11:13:44 -0500

    carga inicial Sección 45 proyecto Gatsby con GraphGL y Dato CMS

---
+++ src/components/header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {css} from '@emotion/react';
+import Navegacion from './nav';
+
+const Header = () => {
+    return ( 
+        <header
+            css={css`
+                background-color: #222;
+                padding: 1rem;
+            `}
+        >
+            <div
+                css={css`
+                    max-width: 80%;
+                    margin: 0 auto;
+
+                    @media (min-width: 768px) {
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                    }
+                `}
+            >
+                <h1
+                    css={css`
+                        color: #fff;
+                        text-align: center;
+                    `}
+                >Hotel Gatsby</h1>
+
+                <Navegacion />
+            </div>
+
+        </header>
+     );
+}
+ 
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,55 +1,70 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react';
+import Helmet from 'react-helmet';
+import {Global, css} from '@emotion/react';
+import Header from './header';
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = (props) => {
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+        <Global 
+            styles={css`
+                /* :root {
+                    --gris: #3d3d3d;
+                    --gris2: #6f6f6f;
+                    --gris3: #e1e1e1;
+                    --naranja: #da552f;
+                } */
+
+                html {
+                    font-size: 62.5%;
+                    box-sizing: border-box;
+                }
+
+                *, *:before, *:after {
+                    box-sizing: inherit;
+                }
+
+                body {
+                    font-size: 1.8rem;
+                    line-height: 1.5;
+                    margin: 0;
+                    font-family: 'PT Sans', sans-serif;
+                }
+
+                h1, h2, h3 {
+                    margin: 0;
+                    line-height: 1.5;
+                }
+
+                h1, h2 {
+                    font-family: 'Roboto Slab', serif;
+                }
+
+                h1 {
+                    font-family: 'PT Sans', sans-serif;
+                }
+
+                ul {
+                    list-style: none;
+                    margin: 0;
+                    padding: 0;
+                }
+            `}
+        />
+
+            {/* =================================== */}
+
+        <Helmet>
+            <title>Gatsby Hotel</title>
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        </Helmet>
+
+        <Header />
+
+        {props.children}
     </>
   )
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
 export default Layout
